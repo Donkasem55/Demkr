a0a0d4635c66bb5057e48a48c53fa3cb4dc3558e
TheLuckyCuber999 <92718237+Donkasem55@users.noreply.github.com>
2026-04-09 13:04:03 +0800

    posting works now

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from wsgiref.simple_server import make_server
-import subprocess, os, sys
+import subprocess, os, sys, json, time
+from datetime import datetime
 
 def start_read_daemon(serverid, msgid):
     p = subprocess.run(
@@ -32,8 +33,8 @@ Hello, welcome to your new community!"""
         stderr=subprocess.PIPE,
         text=True,
         bufsize=1
-    )  
-    return p.stdout, p.stderr
+    )
+    return msgid
 
 def daemon_read(serverid, msgid):
     data, err = start_read_daemon(serverid, msgid)
@@ -61,22 +62,72 @@ def application(environ, start_response):
         response_body = b"Hello, World!"
     elif request_method == 'GET':
         if path[0] == "s":
-            print(path)
-            dat = daemon_read(path[1], path[2])
-            print(dat)
-            print(dat["user"], "said: ", dat["title"])
-            status = dat["code"]
+            if path[-1] == "":
+                path.pop()
+            try:
+                dat = daemon_read(path[1], path[2])
+                status = dat["code"]
+                if status == "404 Not Found":
+                    with open("global.html") as f:
+                        x = f.read()
+                    with open("404.html") as f:
+                        t = f.read()
+                        t = x.replace("{PAGEBODY}", t).replace("{PAGETITLE}", f"Page Not Found | Demkr, the Democratic Central")
+                    response_body = t.encode("utf-8")
+                else:
+                    with open("global.html") as f:
+                        x = f.read()
+                    with open("post.html") as f:
+                        t = f.read()
+                        t = x.replace("{PAGEBODY}", t).replace("{PAGETITLE}", f"{dat["title"]} | Demkr, the Democratic Central")
+                    response_body = f"<a target=\"_top\" href=\"{path[2]}\">{t.replace("{BODYTEXT}", dat["text"]).replace("{TITLE}", dat["title"]).replace("{DATE}", dat["date"]).replace("{USER}", dat["user"])}</a>".encode("utf-8")
+            except IndexError:
+                with open("global.html") as f:
+                    x = f.read()
+                    print(path)
+                    t = x.replace("{PAGEBODY}", "".join([f"<a href=\"{path[1]}/{a}\"><iframe class=\"post\" title=\"\" width=\"100%\" height=\"150px\" src=\"{path[1]}/{a}\"></iframe></a><br>" for a in sorted(os.listdir(f"db/server/{path[1]}"), reverse=True)])).replace("{PAGETITLE}", f"s/{path[1]} | Demkr, the Democratic Central")
+                response_body = t.encode("utf-8")
+                status = '200 OK'
+            except FileNotFoundError:
+                with open("global.html") as f:
+                    x = f.read()
+                with open("404.html") as f:
+                    t = f.read()
+                    t = x.replace("{PAGEBODY}", t).replace("{PAGETITLE}", f"Page Not Found | Demkr, the Democratic Central")
+                response_body = t.encode("utf-8")
+                
+        elif path[0] == "post":
             with open("global.html") as f:
                 x = f.read()
-            with open("post.html") as f:
+            with open("newpost.html") as f:
                 t = f.read()
-                t = x.replace("{PAGEBODY}", t).replace("{PAGETITLE}", f"{dat["title"]} | Demkr, the Democratic Central")
-            response_body = f"{t.replace("{BODYTEXT}", dat["text"]).replace("{TITLE}", dat["title"]).replace("{DATE}", dat["date"]).replace("{USER}", dat["user"])}".encode("utf-8")
+                t = x.replace("{PAGEBODY}", t).replace("{PAGETITLE}", f"New Post | Demkr, the Democratic Central")
+            response_body = t.encode("utf-8")
+            status = '200 OK'
+
+        elif path[0] == "post.js":
+            with open("post.js") as f:
+                x = f.read()
+            response_body = x.encode("utf-8")
+            status = '200 OK'
+            
         else:
             status = '404 Not Found'
-            response_body = b"404 Not Found"
+            with open("global.html") as f:
+                x = f.read()
+            with open("404.html") as f:
+                t = f.read()
+                t = x.replace("{PAGEBODY}", t).replace("{PAGETITLE}", f"Page Not Found | Demkr, the Democratic Central")
+            response_body = t.encode("utf-8")
+
     elif request_method == 'POST':
-        pass
+        request = environ['wsgi.input'].read().decode("utf-8")
+        req = json.loads(request)
+        print(req)
+        date = datetime.now().strftime("%H:%M:%S, %d/%m/%Y")
+        a = daemon_write(req["comm"], req["user"], date, req["title"], req["body"])
+        status = '200 OK'
+        response_body = str(a).encode("utf-8")
 
     else:
         status = '404 Not Found'
--- post.js
+++ post.js
@@ -1,10 +1,25 @@
-function post(user, date, title, body) {
-    const xhr = new XMLHttpRequest();
-    xhr.open('POST', `/data`, true);
-    xhr.onreadystatechange = function () {
-    if (xhr.readyState === 4 && xhr.status === 200) {
-        console.log(JSON.parse(xhr.responseText)); // Parse JSON data
-    }
-    };
-    xhr.send();
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+
+let com = urlParams.get('comm');
+if (urlParams.has("ref")) {
+    window.location.assign(`/s/${com}`);
+}
+
+function post() {
+    var npc = document.getElementById('newpostcomm').value;
+    var npu = document.getElementById('newpostusername').value;
+    var npt = document.getElementById('newposttitle').value;
+    var npb = document.getElementById('newpostbody').value;
+
+    fetch(window.location.href, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ comm: npc, user: npu, title: npt, body: npb })
+    })
+    setTimeout(() => {
+        window.location.href = `/post?comm=${npc}&ref=true`;
+    }, 150);
 }
